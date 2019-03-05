ba31e52f5de7307c09797a16e68498ca31a45205
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-05 10:58:51 +0000

    Deploy dapplion/dappnode-pioneers to github.com/dapplion/dappnode-pioneers.git:gh-pages

---
+++ precache-manifest.86d401165ebc99f1c999fcffb5dcdde8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db33a17a479d0016a4c70515150e1e49",
+    "url": "./static/media/dappnode-logo.db33a17a.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "e877c191c496f90361c9",
+    "url": "./static/js/main.e877c191.chunk.js"
+  },
+  {
+    "revision": "e7f7481c80bfcd8e0671",
+    "url": "./static/js/2.e7f7481c.chunk.js"
+  },
+  {
+    "revision": "e877c191c496f90361c9",
+    "url": "./static/css/main.03f0a84b.chunk.css"
+  },
+  {
+    "revision": "e7f7481c80bfcd8e0671",
+    "url": "./static/css/2.0391fb14.chunk.css"
+  },
+  {
+    "revision": "cdad6411d8ce7548a772337e43125adb",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dappnode-pioneers/precache-manifest.f45969f337b729e2a76fe9b2aa34d75a.js"
+  "./precache-manifest.86d401165ebc99f1c999fcffb5dcdde8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dappnode-pioneers/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
